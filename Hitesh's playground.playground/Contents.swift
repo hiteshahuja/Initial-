c460534297006298c006097ee39c3854a874c1a8
Hitesh Ahuja <hiteshahuja@Hiteshs-MacBook-Pro.local>
2015-09-20 02:59:51 -0700

    swift playground

---
+++ Hitesh's playground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var name = "Hitesh"
+var place = "SFO"
+var num1 = 5
+var num2 = 10
+var num3 = num1 + num2
+print (num1+num2)
